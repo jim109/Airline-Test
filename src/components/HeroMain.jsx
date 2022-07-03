2f3a31ef25f2dc6f637f07798b49759532ba77d9
Jimmy Osma <josma109@gmail.com>
2022-07-03 17:49:08 -0500

    update layout mainApp

--- src/components/HeroMain.jsx
+++ src/components/HeroMain.jsx
@@ -1,25 +1,24 @@
 import { CardOffers } from "./cardOffers/CardOffers"
+import { MenuCards } from "./cardOffers/MenuCards"
 
 
 export const HeroMain = () =>{
 
     return(
         <section className="heromain">
-            <h2>Offers</h2> 
-            <div className="heromain_cards">
+            <h2 className="g1">Offers</h2>
+            <div className="g5"></div>            
+            <div className="heromain_cards g2">
                 <CardOffers imgOffers={ "./assets/moscow.jpg"} city={"Moscow"} price={"$600,86"}/>
                 <CardOffers imgOffers={ "./assets/puerto-rico.jpg"} city={"Puerto Rico"} price={"$600,86"}/>
-                <CardOffers imgOffers={ "./assets/honduras.jpg"} city={"Honduras"} price={"$600,86"}/>
+                <CardOffers imgOffers={ "./assets/honduras.jpg"} city={""} price={"$600,86"}/>
             </div>      
             
-            <nav className="heromain_nav">
-                <li>Check-in</li>
-                <li>Manage My Booking</li>
-                <li>Search</li>
-                <li>Destinations</li>
-                <li>Members</li>
-            </nav>
-            <p>Chosen destination</p>
+            <MenuCards className="g3" />
+            <div className="heromain_text g4">
+                <p>Trip to Honduras</p>
+                <p className="heromain_text--destin">Chosen destination</p>
+            </div>            
         </section>
     )
 }
---
+++ src/components/cardOffers/MenuCards.jsx
@@ -0,0 +1,21 @@
+
+
+export const MenuCards = () =>{
+    return(
+        <nav className="menucards">
+            <h3>Menu</h3>
+            <ul className="menucards_menu">
+                <li><a href="/">Check-in</a></li>
+                <li><a href="/">Manage My Booking</a></li>
+                <li><a href="/">Search</a></li>
+                <li><a href="/">Destinations</a></li>
+                <li><a href="/">Members</a></li>
+            </ul>
+            <div className="triangulo"></div>
+            
+                 
+            
+        </nav>
+    )
+   
+}
